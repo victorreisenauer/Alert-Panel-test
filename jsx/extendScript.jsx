651314fff3eec339aa83c7b2340f818acb80c2c5
victorreisenauer <victorreisenauer99@gmail.com>
2024-03-12 13:15:03 +0100

    Initial commit

---
+++ jsx/extendScript.jsx
@@ -0,0 +1,15 @@
+var proj = app.project;
+alert(proj.name)
+
+
+$.runScript = {
+
+
+	changeThisFunctionName: function() {
+	//--------------------------------------------------------------------------------------------------------------------------------------------------
+		//Copy/Paste your script from Adobe Extendscript HERE!!!!
+		alert("Please find and read the ReadMe.txt file for instruction on how to use this panel template.")
+	//--------------------------------------------------------------------------------------------------------------------------------------------------
+}	
+    
+}
